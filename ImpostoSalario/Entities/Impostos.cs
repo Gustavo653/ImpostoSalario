1c2ba8e1bebdb0a0f0970889a919acc6aa704eb5
Gustavo653 <gustavohs2004@gmail.com>
2021-02-16 19:48:59 -0300

    Projeto de impostos

---
+++ ImpostoSalario/Entities/Impostos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoSalario.Entities
+{
+    class Impostos
+    {
+        public double Salario { get; set; }
+
+
+        public Impostos(double salario)
+        {
+            Salario = salario;
+        }
+
+        public double CalcularFgts(double salario)
+        {
+            double fgts = salario * 0.08;
+            return fgts;
+        }
+
+        public double CalcularVale(double salario)
+        {
+            double vale = salario * 0.06;
+            return vale;
+        }
+
+        public double CalcularInss(double salario)
+        {
+            double inss = salario * 0.08;
+            return inss;
+        }
+
+        public double CalcularIr(double salario)
+        {
+            double ir = salario * 0.2;
+            if (salario <= 1900)
+            {
+                return 0;
+            }
+            else
+            {
+                return ir;
+            }
+
+        }
+
+        public double CalcularSalario(double salario)
+        {
+            double inss = salario * 0.08;
+            double vale = salario * 0.06;
+            if (salario <= 1900)
+            {
+                salario = salario - (inss + vale);
+                return salario;
+            }
+            else
+            {
+                salario = salario - (vale + inss + salario * 0.2);
+                return salario;
+            }
+        }
+    }
+}
---
+++ ImpostoSalario/Program.cs
@@ -0,0 +1,29 @@
+﻿using ImpostoSalario.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu salário bruto: ");
+            double salario = double.Parse(Console.ReadLine());
+            Impostos impostos = new Impostos(salario);
+
+            Console.WriteLine();
+            Console.WriteLine("Seu salário bruto: " + salario);
+            Console.WriteLine("O valor do seu fgts é: " + impostos.CalcularFgts(salario).ToString());
+            Console.WriteLine("O valor do seu vale é: " + impostos.CalcularVale(salario).ToString());
+            Console.WriteLine("O valor do seu inss é: " + impostos.CalcularInss(salario).ToString());
+            Console.WriteLine("O valor do seu ir é: " + impostos.CalcularIr(salario).ToString());
+            Console.WriteLine("Seu salário é: " + impostos.CalcularSalario(salario).ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
